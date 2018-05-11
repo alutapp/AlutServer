1b1d2014d0e5d21443a9e8bac8f030e36676ab1f
zoharmo <morz3212@gmail.com>
2018-05-11 19:10:48 +0300

    node modules & app.ls mor & gidi changes - add get user rest service

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 var http = require('http'),
     db = require('./model/db'),
     user = require('./dataAccess/userDao')
+    const express = require('express')
+    const app = express()
 
-  user.createUser(function(err){
-    if (err) {
-      console.log('*** Err' + err);
-    }});
+
+ 
+
+    app.get("/SignIn/:email", function (req, res) {
+      user.getUser(req.params.email ,function(err, data){
+        console.log("**user ** ="+  data);
+
+        res.send(data);
+
+        if (err) {
+          console.log('*** Err' + err);
+      }});
+   })
+
+   var server = app.listen(3000, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+  
+    console.log("Example app listening at http://%s:%s", host, port)
+  
+  })
+
+  //user.createUser(function(err){
+    //if (err) {
+     // console.log('*** Err' + err);
+   // }});
 
